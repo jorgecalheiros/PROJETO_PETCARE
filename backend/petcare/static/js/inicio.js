1a97ff423204b33beee5d78d99e51e688a3f1a7d
Jorge Calheiros <86486202+jorgecalheiros@users.noreply.github.com>
2024-11-24 11:13:43 -0300

    Update inicio.js
    Update inicio.js

--- backend/petcare/static/js/inicio.js
+++ backend/petcare/static/js/inicio.js
@@ -1,6 +1,6 @@
 import {makeGetRequest, setAuthorizationTokenHeader, userIsRegistered, tratamentosDeErros} from "./utils/api-utils.js";
 import {ROUTES_API, ROUTES_SITE} from "./utils/global.js";
-import { buttonLogout } from "./utils/site-utils.js";
+import { logout } from "./utils/site-utils.js";
 
 userIsRegistered((response) => {}, (response) => {
     tratamentosDeErros.owner.register.donthaveregister(response);
@@ -9,40 +9,14 @@ userIsRegistered((response) => {}, (response) => {
 
 document.addEventListener("DOMContentLoaded", async function(){
     const headers = setAuthorizationTokenHeader();
-    const listaPets = $("#lista-pets").empty(); 
+    const totalpets = $("#total-pets").empty(); 
     const buttonLogout = document.getElementById("logout");
 
     logout(buttonLogout);
 
     await makeGetRequest(ROUTES_API.get_pets, headers, async(response) => {
         const data = await response.json();
-        if(data.length == 0){
-            listaPets.append(`
-                <h3 class="col-lg-6">Sem pets cadastrados.</h3>
-                `);
-        }
-        data.forEach(function(item){
-            const linha = `
-
-                <div class="col">
-                    <div class="card">
-                        <div class="card-body text-center">
-                          <h5 class="card-title">${item.name}</h5>
-                          <p class="card-text text-muted">
-                            Raça: ${item.race} <br/>
-                            Especie: ${item.species} <br/>
-                            Peso: ${item.weight} kg<br/>
-                          </p>
-                          <div class="d-flex justify-content-center">
-                            <a href="#" class="btn btn-default">Historico Medico</a>
-                          </div>
-                        </div>
-                    </div>
-                </div>
-            
-            `;
-            listaPets.append(linha);
-        })
+        totalpets.append(data.length);
     });
     
 });
